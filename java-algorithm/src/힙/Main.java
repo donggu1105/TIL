80b6c039012faed8a88992da568e15f97f05f527
kdh_donggu <kdh.candoanything@thecommerce.co.kr>
2021-12-12 18:58:49 +0900

    heap

---
+++ java-algorithm/src/힙/Main.java
@@ -0,0 +1,15 @@
+package 힙;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Heap heap = new Heap(15);
+        heap.insert(10);
+        heap.insert(8);
+        heap.insert(5);
+        heap.insert(4);
+        heap.insert(20);
+        System.out.println(heap.heapArray);
+    }
+}
