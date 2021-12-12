8f75c23d4820d22c57fda6105378df67b9dd0f97
kdh_donggu <kdh.candoanything@thecommerce.co.kr>
2021-12-12 17:20:20 +0900

    tree

---
+++ java-algorithm/src/트리/Main.java
@@ -0,0 +1,33 @@
+package 트리;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Case3-1: 삭제할 Node가 Child Node를 두 개 가지고 있을 경우
+        NodeMgmt myTree = new NodeMgmt();
+        myTree.insertNode(10);
+        myTree.insertNode(15);
+        myTree.insertNode(13);
+        myTree.insertNode(11);
+        myTree.insertNode(14);
+        myTree.insertNode(18);
+        myTree.insertNode(16);
+        myTree.insertNode(19);
+        myTree.insertNode(17);
+        myTree.insertNode(7);
+        myTree.insertNode(8);
+        myTree.insertNode(6);
+        System.out.println(myTree.delete(15));
+        System.out.println("HEAD: " + myTree.head.value);
+        System.out.println("HEAD LEFT: " + myTree.head.left.value);
+        System.out.println("HEAD LEFT LEFT: " + myTree.head.left.left.value);
+        System.out.println("HEAD LEFT RIGHT: " + myTree.head.left.right.value);
+
+        System.out.println("HEAD RIGHT: " + myTree.head.right.value);
+        System.out.println("HEAD RIGHT LEFT: " + myTree.head.right.left.value);
+        System.out.println("HEAD RIGHT RIGHT: " + myTree.head.right.right.value);
+
+        System.out.println("HEAD RIGHT RIGHT LEFT: " + myTree.head.right.right.left.value);
+        System.out.println("HEAD RIGHT RIGHT RIGHT: " + myTree.head.right.right.right.value);
+    }
+}
