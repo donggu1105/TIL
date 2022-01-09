ab51c43bcc96944e9cec5c4ac447e31180c2f7ad
donggu1105 <kbh11051@gmail.com>
2022-01-09 12:27:23 +0900

    병합정렬

---
+++ java-algorithm/src/알고리즘/고급정렬알고리즘/병합정렬/Split.java
@@ -0,0 +1,34 @@
+package 알고리즘.고급정렬알고리즘.병합정렬;
+
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.regex.PatternSyntaxException;
+
+public class Split {
+
+    public static void main(String[] args) {
+
+        Split split = new Split();
+
+        split.splitFunc(new ArrayList<Integer>(Arrays.asList(4,1,3,2,5,6)));
+    }
+
+    public void splitFunc(ArrayList<Integer> dataList) {
+        if (dataList.size() <= 1) {
+            return;
+        }
+
+        int medium = dataList.size() / 2;
+
+        ArrayList<Integer> leftArr = new ArrayList<Integer>();
+        ArrayList<Integer> rightArr = new ArrayList<Integer>();
+
+        // 0 부터 medium - 1 인덱스 번호까지 해당 배열 아이템을 서브배열
+        leftArr = new ArrayList<Integer>(dataList.subList(0, medium));
+        rightArr = new ArrayList<>(dataList.subList(medium, dataList.size()));
+
+        System.out.println(leftArr);
+        System.out.println(rightArr);
+    }
+}
