fc537ae47a16b022af4f516dca8c5c3ab82fb4b6
donggu1105 <kbh11051@gmail.com>
2022-01-13 21:55:00 +0900

    123

---
+++ java-algorithm/src/알고리즘/정렬알고리즘몸풀기/이진탐색/BinarySearch.java
@@ -0,0 +1,36 @@
+package 알고리즘.정렬알고리즘몸풀기.이진탐색;
+
+import java.util.ArrayList;
+
+public class BinarySearch {
+
+    public boolean searchFunc(ArrayList<Integer> dataList, Integer searchItem) {
+
+        // exceptions
+        if (dataList.size() == 0) {
+            return false;
+        }
+
+        if (dataList.size() == 1 && searchItem == dataList.get(0)) {
+            return true;
+        }
+
+        if (dataList.size() == 1 && searchItem != dataList.get(0)) {
+            return false;
+        }
+
+        int medium = dataList.size() / 2;
+
+        if (searchItem == dataList.get(medium)) {
+            return true;
+        } else {
+            if (searchItem < dataList.get(medium)) {
+                return this.searchFunc(new ArrayList<>(dataList.subList(0, medium)), searchItem);
+            } else {
+                return this.searchFunc(new ArrayList<>(dataList.subList(medium, dataList.size())), searchItem);
+
+            }
+        }
+
+    }
+}
---
+++ java-algorithm/src/자료구조/트리/연습/TreePractice1.java
@@ -0,0 +1,88 @@
+package 자료구조.트리.연습;
+
+public class TreePractice1 {
+
+
+
+    public static class NodeMgmt {
+        Node head = null;
+
+        public static class Node {
+            int value;
+            Node left;
+            Node right;
+
+            public Node(int data) {
+                this.value = data;
+                this.left = null;
+                this.right = null;
+            }
+        }
+        public Node search(int data) {
+            System.out.println("Test");
+            // case 1: head가 없을떄
+            if (this.head == null) {
+                return null;
+            } else {
+                // case 2: 노드가 하나 이상 있을떄
+                Node findNode = this.head;
+                while (findNode != null) {
+                    if (findNode.value == data) {
+                        return findNode;
+                    } else {
+                        if (data < findNode.value) {
+                            findNode = findNode.left;
+                        } else {
+                            findNode = findNode.right;
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+        public boolean insertNode(int data) {
+            // CASE 1 : head is null
+            if (this.head == null) {
+                this.head = new Node(data);
+            } else {
+                // CASE 2 : 왼쪽에 들어 갈때
+                Node findNode = this.head;
+                while (true) {
+                    if (data < findNode.value) {
+                        if (findNode.left == null) {
+                            findNode.left = new Node(data);
+                            break;
+                        } else {
+                            findNode = this.head.left;
+                        }
+                    // CASE 3: 오른쪽에 들어갈때
+                    } else {
+                        if (findNode.right == null) {
+                            findNode.right = new Node(data);
+                            break;
+                        } else {
+                            findNode = this.head.right;
+                        }
+                    }
+                }
+
+
+            }
+            return true;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        NodeMgmt nodeMgmt = new NodeMgmt();
+
+        System.out.println(nodeMgmt.insertNode(1));
+        nodeMgmt.insertNode(2);
+        nodeMgmt.insertNode(4);
+        nodeMgmt.insertNode(16);
+
+        nodeMgmt.search(2);
+//        System.out.println(findNode.value);
+
+    }
+}
