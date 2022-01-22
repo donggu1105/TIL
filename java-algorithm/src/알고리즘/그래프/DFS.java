4400aac5ea8f52d125984b35fdcb6d06cfb28935
donggu1105 <kbh11051@gmail.com>
2022-01-22 14:33:09 +0900

    DFS

--- java-algorithm/src/알고리즘/그래프/DFS.java
+++ java-algorithm/src/알고리즘/그래프/DFS.java
@@ -1,6 +1,52 @@
 package 알고리즘.그래프;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
 // Depth First Search
 public class DFS {
 
+
+    public ArrayList<String> DFSSearch(HashMap<String, ArrayList<String>> graph, String startNode) {
+
+        ArrayList<String> visited = new ArrayList<>();
+        ArrayList<String> needVisit = new ArrayList<>();
+
+        needVisit.add(startNode);
+        while (needVisit.size() > 0) {
+            String node = needVisit.remove(needVisit.size() - 1);
+            if (!visited.contains(node)) {
+                visited.add(node);
+                needVisit.addAll(graph.get(node));
+            }
+        }
+        return visited;
+
+    }
+    public static void main(String[] args) {
+
+
+        HashMap<String, ArrayList<String>> graph = new HashMap<>();
+
+        graph.put("A", new ArrayList<String>(Arrays.asList("B", "C")));
+        graph.put("B", new ArrayList<String>(Arrays.asList("A", "D")));
+        graph.put("C", new ArrayList<String>(Arrays.asList("A", "G", "H", "I")));
+        graph.put("D", new ArrayList<String>(Arrays.asList("B", "E", "F")));
+        graph.put("E", new ArrayList<String>(Arrays.asList("D")));
+        graph.put("F", new ArrayList<String>(Arrays.asList("D")));
+        graph.put("G", new ArrayList<String>(Arrays.asList("C")));
+        graph.put("H", new ArrayList<String>(Arrays.asList("C")));
+        graph.put("I", new ArrayList<String>(Arrays.asList("C", "J")));
+        graph.put("J", new ArrayList<String>(Arrays.asList("I")));
+
+
+        DFS dfs = new DFS();
+
+        ArrayList<String> result = dfs.DFSSearch(graph, "A");
+
+        System.out.println(result);
+
+    }
 }
