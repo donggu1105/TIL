cfdffeeca362206c5dafe738858a71f1f0075924
joeyKang <kdh.candoanything@thecommerce.co.kr>
2022-04-05 10:20:04 +0900

    practice2

--- java-algorithm/src/알고리즘/연습/Practice2.java
+++ java-algorithm/src/알고리즘/연습/Practice2.java
@@ -7,30 +7,46 @@ public class Practice2 {
 
     static FastReader scan = new FastReader();
     static StringBuilder sb = new StringBuilder();
-    // 중복 허용 ,순서 없음
-    // 4개의 숫자중 2개로 이루어진 쌍을 구해라 여러번골라도되고, 순서도 없음  1, 1, 1, 2
+
+    /*
+    문제
+자연수 N과 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오.
+
+1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열
+입력
+첫째 줄에 자연수 N과 M이 주어진다. (1 ≤ M ≤ N ≤ 8)
+
+출력
+한 줄에 하나씩 문제의 조건을 만족하는 수열을 출력한다. 중복되는 수열을 여러 번 출력하면 안되며, 각 수열은 공백으로 구분해서 출력해야 한다.
+
+수열은 사전 순으로 증가하는 순서로 출력해야 한다.
+
+    중복 X, 순서 X
+    * **/
     static int N, M;
-    static int[] selected;
+    static int[] selected, used;
 
     static void input() {
-
         N = scan.nextInt();
         M = scan.nextInt();
         selected = new int[M + 1];
-
+        used = new int[N + 1];
     }
 
 
     static void rec_func(int k) {
-
         if (k == M + 1) {
             for (int i = 1; i <= M; i++) sb.append(selected[i]).append(" ");
             sb.append("\n");
         } else {
             for (int cand = 1; cand <= N; cand++) {
-                selected[k] = cand;
-                rec_func(k + 1);
-                selected[k] = 0;
+
+                if (used[cand] == 1) continue;
+
+                selected[k] = cand; used[cand] = 1;
+
+                rec_func(k+1);
+                selected[k] = 0; used[cand] = 0;
             }
         }
     }
