708f8fc1c7ec76fe89692324c17c39b0da588b9b
donggu <freelancer7074@gmail.com>
2022-07-24 18:15:48 +0900

    123

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+
+    <todo-header></todo-header>
+    <todo-input></todo-input>
+    <todo-list></todo-list>
+    <todo-footer></todo-footer>
+
+
+  </div>
+</template>
+
+<script>
+
+import TodoHeader from "@/components/TodoHeader";
+import TodoFooter from "@/components/TodoFooter";
+import TodoInput from "@/components/TodoInput";
+import TodoList from "@/components/TodoList";
+export default {
+  components: {TodoList, TodoInput, TodoFooter, TodoHeader}
+}
+</script>
+
+<style>
+
+  body{
+    text-align: center;
+    background-color: #F6F6F6;
+  }
+
+  input {
+    border-style: groove;
+    width: 200px;
+  }
+
+  button {
+    border-style: groove;
+  }
+
+  .shadow {
+    box-shadow: 5px 10px 1px rgba(0, 0, 0, 0.03);
+
+  }
+</style>
---
+++ vue-todo/src/components/TodoInput.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <input type="text" v-model="newTodoItem">
+    <button v-on:click="addTodo"> add </button>
+  </div>
+</template>
+
+
+<script>
+
+  export default {
+
+    data : function() {
+      return {
+        newTodoItem: 'gggg3'
+      }
+    },
+    methods: {
+      addTodo: function () {
+        localStorage.setItem(this.newTodoItem, this.newTodoItem);
+        this.newTodoItem = '';
+      },
+    }
+
+  }
+
+</script>
+
+<style></style>
