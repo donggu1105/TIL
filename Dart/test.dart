334b1048bc1da283722c5b0f6e4c3666d4d34970
donggu1105 <kbh11051@gmail.com>
2022-11-27 13:10:29 +0900

    dynamic

--- Dart/test.dart
+++ Dart/test.dart
@@ -1,16 +1,9 @@
 void main(){
-
-  print("hello code factory");
-
-  var name = "name";
-  print(name);
-
-  var name2 = "레드벨벳";
-  print(name2);
-
-  name = "changed";
-  print(name);
-
+  
+  DateTime now = DateTime.now();
+  
+  
+  print(now);
 
 
 
