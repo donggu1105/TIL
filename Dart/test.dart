80214d17364be27d04ab1bf61a19128599ecf4c4
donggu1105 <kbh11051@gmail.com>
2022-12-04 08:42:41 +0900

    init

--- Dart/test.dart
+++ Dart/test.dart
@@ -1,16 +1,5 @@
 void main(){
   
-  int number = 2;
-
-
-  print(number);
-
-  print(number + 2);
-
-
-  print('------');
-
-
 
 
 
---
+++ Swift/MyApp/TestGame/TestGame/RPS.swift
@@ -0,0 +1,8 @@
+//
+//  RPS.swift
+//  TestGame
+//
+//  Created by 강동현 on 2022/12/04.
+//
+
+import Foundation
