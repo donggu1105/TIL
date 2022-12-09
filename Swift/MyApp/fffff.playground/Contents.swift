b0e6ee12610bbe08be8d44b8a51c016393d0ec4d
donggu <kbh11051@gmail.com>
2022-12-09 10:05:08 +0900

    closure

--- Swift/MyApp/fffff.playground/Contents.swift
+++ Swift/MyApp/fffff.playground/Contents.swift
@@ -1,22 +1,29 @@
 import UIKit
 
-func closureParamFunction(closure: () -> Void) {
-    print("print started")
-    
+
+func closureParamFunction(a:Int, closure: () -> Void) {
+    print(a)
     closure()
 }
 
+closureParamFunction (a: 3) {
+    print("closure")
+}
 
-
-func printStopped() {
-    print("print stopped")
+func test (a: Int, b: Int, closure: (Int) -> Void) {
+    let c = a + b
+    closure(c)
 }
 
 
-closureParamFunction(closure: printStopped)
+test(a: 2, b: 4) {number in print(number)}
 
 
+func performClosure(param: (String) -> Int) {
+    param("Swift")
+}
 
-closureParamFunction(closure: {
-    print("testste")
-})
+
+performClosure {
+        return $0.count
+}
