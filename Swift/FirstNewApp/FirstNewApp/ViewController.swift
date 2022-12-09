eeeb1605ef2d251624763aadd9662be0e13bcd8d
donggu <kbh11051@gmail.com>
2022-12-09 19:00:35 +0900

    app

--- Swift/FirstNewApp/FirstNewApp/ViewController.swift
+++ Swift/FirstNewApp/FirstNewApp/ViewController.swift
@@ -12,9 +12,10 @@ class ViewController: UIViewController {
     
     
     @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var slider: UISlider!
     
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -25,14 +26,23 @@ class ViewController: UIViewController {
     
     func configureUI() {
         mainLabel.text = "초를 선택하세요"
+        // slider 가운데 설정
+        slider.setValue(0.5, animated: true)
+        slider.setThumbImage(UIImage(named: "icons8-thumbs-up-48"), for: .normal)
+        
 
     }
 
     @IBAction func sliderChanged(_ sender: UISlider) {
         
+        let seconds = Int(slider.value * 60)
+        
+        mainLabel.text = "\(seconds)초"
         
     }
     @IBAction func resetButtonTapped(_ sender: UIButton) {
+        mainLabel.text = "초를 선택하세요"
+        slider.value = 0.5
     }
     @IBAction func startButtonTapped(_ sender: UIButton) {
     }
