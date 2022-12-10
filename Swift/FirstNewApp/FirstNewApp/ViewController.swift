099d7e82dc4317e94242a6610571801da59ef710
donggu1105 <kbh11051@gmail.com>
2022-12-10 23:35:45 +0900

    my first app

--- Swift/FirstNewApp/FirstNewApp/ViewController.swift
+++ Swift/FirstNewApp/FirstNewApp/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
@@ -14,6 +15,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var mainLabel: UILabel!
     @IBOutlet weak var slider: UISlider!
     
+    weak var timer: Timer?
+
+    var number: Int = 0
     
     
     override func viewDidLoad() {
@@ -38,13 +42,36 @@ class ViewController: UIViewController {
         let seconds = Int(slider.value * 60)
         
         mainLabel.text = "\(seconds)초"
+        number = seconds
         
     }
     @IBAction func resetButtonTapped(_ sender: UIButton) {
         mainLabel.text = "초를 선택하세요"
         slider.value = 0.5
+        number = 0
+        timer?.invalidate()
     }
+    
     @IBAction func startButtonTapped(_ sender: UIButton) {
+        
+        timer?.invalidate()
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [self] _  in
+            
+            if number > 0 {
+                number -= 1
+                
+                slider.value = Float(number) / Float(60)
+                mainLabel.text = "\(number) 초"
+            } else {
+                number = 0
+                timer?.invalidate()
+                
+                AudioServicesPlayAlertSound(SystemSoundID(1322))
+            }
+
+            
+        }
     }
     
     
