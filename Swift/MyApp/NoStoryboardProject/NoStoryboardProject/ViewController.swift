30e15873234f1fcf8c2a989d16770f5c8d49f081
donggu1105 <kbh11051@gmail.com>
2022-12-12 23:09:27 +0900

    swiftui

--- Swift/MyApp/NoStoryboardProject/NoStoryboardProject/ViewController.swift
+++ Swift/MyApp/NoStoryboardProject/NoStoryboardProject/ViewController.swift
@@ -6,15 +6,67 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        self.view.backgroundColor = .systemRed
+        self.view.backgroundColor = UIColor.white
+        
+        let yellowView = UIView()
+        yellowView.backgroundColor = UIColor.yellow
+//        yellowView.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.view.addSubview(yellowView)
+                
+        yellowView.snp.makeConstraints{ (make) -> Void in
+            make.width.equalTo(100)
+            make.height.equalTo(100)
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview().offset(300)
+            
+        }
+        
+//    NSLayoutConstraint.activate([
+//        yellowView.widthAnchor.constraint(equalToConstant: 100),
+//        yellowView.heightAnchor.constraint(equalToConstant: 100),
+//        yellowView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+//        yellowView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 100)
+//    ])
+//
+        
+        
+        
+        
+        
     }
 
-
 }
+
+
+#if DEBUG
+import SwiftUI
+
+struct ViewControllerPresentable: UIViewControllerRepresentable {
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+            
+    }
+    
+    func makeUIViewController(context: Context) -> some UIViewController {
+        ViewController()
+    }
+}
+
+struct ViewControllerPrepresentable_PreviewProvider: PreviewProvider {
+    static var previews: some View {
+        ViewControllerPresentable()
+    }
+}
+
+#endif
+
+
 
