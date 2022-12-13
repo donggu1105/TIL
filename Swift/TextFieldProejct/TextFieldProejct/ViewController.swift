eec4ecdd30d9b68f7047bffe3cba82c483d5d77a
donggu <kbh11051@gmail.com>
2022-12-13 16:47:48 +0900

    delegate pattern

--- Swift/TextFieldProejct/TextFieldProejct/ViewController.swift
+++ Swift/TextFieldProejct/TextFieldProejct/ViewController.swift
@@ -9,12 +9,7 @@ import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate {
 
-    
-    
-    
     @IBOutlet weak var textField: UITextField!
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,12 +24,15 @@ class ViewController: UIViewController, UITextFieldDelegate {
         
         view.backgroundColor = UIColor.gray
         textField.backgroundColor = .white
-        textField.keyboardType = UIKeyboardType.numberPad
+        textField.keyboardType = UIKeyboardType.default
         textField.placeholder = "전화번호 입력"
         textField.textColor = .black
         textField.borderStyle = .line
         textField.clearButtonMode = .always
         textField.returnKeyType = .next
+        
+        
+        textField.becomeFirstResponder()
     }
     
     // 텍스트필드의 입력을 시작할때 호출 (시작할지 말지 여부 )
@@ -56,12 +54,36 @@ class ViewController: UIViewController, UITextFieldDelegate {
     // 텍스트 필드 글자 내용이 (한글자 한글자) 입력되거나 지워질때 호출이 됨
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         
-        print(string)
-        return true
+        if Int(string) != nil {
+            return false
+        }
+        
+        
+        let maxLength = 10
+        let currentString: NSString = (textField.text ?? "") as NSString
+        let newString: NSString = currentString.replacingCharacters(in: range, with: string) as NSString
+        return newString.length <= maxLength
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        if textField.text == "" {
+            textField.placeholder = "type something"
+            return false
+        } else {
+            return true
+        }
+         
     }
 
 
     @IBAction func buttonTapped(_ sender: Any) {
+        
+        
+//        textField.resignFirstResponder()
+        
+        self.view.endEditing(true)
     }
     
     
