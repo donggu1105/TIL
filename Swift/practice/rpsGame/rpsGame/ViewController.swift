c6d7c607d2f53e27d79f80f2626ba280e93b0026
donggu <kbh11051@gmail.com>
2022-12-14 16:42:22 +0900

    app3

---
+++ Swift/practice/rpsGame/rpsGame/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  rpsGame
+//
+//  Created by web_dev on 2022/12/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var mainLabel: UILabel!
+    
+    @IBOutlet weak var comImageView: UIImageView!
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    
+    @IBOutlet weak var comChoiceLabel: UILabel!
+    @IBOutlet weak var myChoiceLabel: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func rpsButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func selectButtonTapped(_ sender: UIButton) {
+    }
+    
+    
+    
+    @IBAction func resetButtonTapped(_ sender: UIButton) {
+    }
+    
+    
+}
+
+
