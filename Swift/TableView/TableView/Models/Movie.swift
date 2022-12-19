404dbfa8134d2863355784a22fdc942071866246
donggu <kbh11051@gmail.com>
2022-12-19 17:56:08 +0900

    7th app

---
+++ Swift/TableView/TableView/Models/Movie.swift
@@ -0,0 +1,20 @@
+//
+//  Movie.swift
+//  TableView
+//
+//  Created by web_dev on 2022/12/19.
+//
+
+import UIKit
+
+struct Movie {
+    
+    
+    var movieImage: UIImage?
+    
+    let movieName: String
+    
+    let movieDescription: String
+    
+    
+}
---
+++ Swift/TableView/TableView/Views/MovieCell.swift
@@ -0,0 +1,35 @@
+//
+//  MovieCell.swift
+//  TableView
+//
+//  Created by web_dev on 2022/12/19.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    
+    @IBOutlet weak var mainImageView: UIImageView!
+    
+    @IBOutlet weak var movieNameLabel: UILabel!
+
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    
+    
+
+}
