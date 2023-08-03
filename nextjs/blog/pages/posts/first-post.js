e0e3e3fbde4564873cfef7e2139aba70c340e556
donggu1105 <kbh11051@gmail.com>
2023-08-04 05:43:32 +0900

    blog with nextjs

---
+++ nextjs/blog/pages/posts/first-post.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import Layout from "../../components/Layout";
+
+export default function FirstPost() {
+    return (
+    <Layout>
+    <Head>
+        <title>첫번째 글</title>
+    </Head>
+    <h1> First Post </h1>
+    </Layout>
+    )
+
+};
