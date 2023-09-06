6025226c5a570092e09354046d255d007a787ffd
donggu1105 <kbh11051@gmail.com>
2023-09-07 06:58:11 +0900

    disney plus app image 추가

--- React.js/react-disney-plus-app/src/components/Nav.js
+++ React.js/react-disney-plus-app/src/components/Nav.js
@@ -1,9 +1,52 @@
 import React from 'react';
+import styled from "styled-components";
 
 function Nav(props) {
     return (
-        <div></div>
+        <NavWrapper>
+            <Logo>
+                <img alt="Disney Plus Logo"
+                     src="/images/logo.svg"
+                     onClick={() => (window.location.href = "/")}
+                />
+
+
+            </Logo>
+
+
+        </NavWrapper>
     );
 }
 
 export default Nav;
+
+const NavWrapper = styled.nav`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 70px;
+  background-color: #090b13;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 36px;
+  letter-spacing: 16px;
+  z-index: 3;
+`;
+
+const Logo = styled.a`
+  padding: 0;
+  width: 80px;
+  margin-top: 4px;
+  max-height: 70px;
+  font-size: 0;
+  display: inline-block;
+  
+  img {
+    display: block;
+    width: 100%;
+  }
+
+
+`;
