b4c0d437748036cb7cea8b4e64586bd914f65c0b
donggu1105 <kbh11051@gmail.com>
2023-09-09 21:39:15 +0900

    sytled componet css

--- React.js/react-disney-plus-app/src/components/Banner.js
+++ React.js/react-disney-plus-app/src/components/Banner.js
@@ -1,7 +1,9 @@
 import React, {useEffect, useState} from 'react';
 import requests from "../api/requests";
 import axios from "../api/axios";
-
+import "./Banner.css"
+import {click} from "@testing-library/user-event/dist/click";
+import styled from "styled-components";
 function Banner() {
 
 
@@ -9,8 +11,12 @@ function Banner() {
         fetchData();
     }, []);
 
-    const [movie, setMovie] = useState();
+    const [movie, setMovie] = useState([]);
+    const [isClicked, setIsClicked] = useState(false)
+
     const fetchData = async () => {
+
+
         const request =  await axios.get(requests.fetchNowPlaying);
 
 
@@ -28,9 +34,101 @@ function Banner() {
     }
 
 
+    const truncate = (str, n) => {
+        return str?.length > n ? str.substring(0, n) + "..." : str;
+    }
+
+
+    if (isClicked) {
+        return (
+            <>
+            <Container>
+                <HomeContainer>
+                    <Iframe
+                        src={`https://www.youtube.com/embed/${movie.videos.results[0].key}?controls=0&autoplay=1&loop=1&mute=1&playlist=${movie.videos.results[0].key}`}
+                        width="640"
+                        height="360"
+                        frameborder="0"
+                        allow="autoplay; fullscreen"
+                    ></Iframe>
+                </HomeContainer>
+            </Container>
+            <button onClick={() => setIsClicked(false)}>X</button>
+            </>
+        );
+
+    }
+
     return (
-        <div></div>
+        <header
+            className="banner"
+            style={{
+                backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+                backgroundPosition: "top center",
+                backgroundSize: "cover",
+            }}
+        >
+
+            <div className="banner__contents">
+                <h1 className="banner__title">
+                    {movie.title || movie.name || movie.original_name}
+                </h1>
+
+                <div className="banner__buttons">
+                    {movie?.videos?.results[0]?.key &&
+                        <button className="banner__button play"
+                            onClick={() => setIsClicked(true)}
+                        >
+                            Play
+                        </button>
+                    }
+                </div>
+
+                <h1 className="banner__description">
+                    {truncate(movie.overview, 100)}
+                </h1>
+            </div>
+
+            <div className="banner--fadeBottom" />
+        </header>
     );
 }
 
 export default Banner;
+
+
+const Container = styled.div`
+    
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    width: 100%;
+    height: 100vh;
+`;
+
+
+const HomeContainer = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+
+const Iframe = styled.iframe`
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  opacity: 0.65;
+  border: none;
+  
+  &::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+
+
+`
