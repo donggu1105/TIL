fa8bd8b5562480bca6a9c7eb8ea5be30c6e31818
donggu1105 <kbh11051@gmail.com>
2023-10-01 07:40:11 +0900

    disney swiper fixed

--- React.js/FastCampus-Online-Frontend-Signature/1_react/react-disney-plus-app/src/firebase.js
+++ React.js/FastCampus-Online-Frontend-Signature/1_react/react-disney-plus-app/src/firebase.js
@@ -1,20 +1,25 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import "firebase/auth"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCRIQfz9cbtyLPRnSnnhOKN6IePyJtoiZg",
-  authDomain: "react-disney-plus-app.firebaseapp.com",
-  projectId: "react-disney-plus-app",
-  storageBucket: "react-disney-plus-app.appspot.com",
-  messagingSenderId: "17555251393",
-  appId: "1:17555251393:web:672c0357240c85d4b9f440"
+  apiKey: "AIzaSyAh1xLZXKT3Em6Ey7lbuB8E5NdDfRTB9x4",
+  authDomain: "react-disney-app-donggu.firebaseapp.com",
+  projectId: "react-disney-app-donggu",
+  storageBucket: "react-disney-app-donggu.appspot.com",
+  messagingSenderId: "613464304431",
+  appId: "1:613464304431:web:f729fb6d900d002d244fda",
+  measurementId: "G-SHX658GERW"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 
 export default app;
--- React.js/react-disney-plus-app/src/components/Nav.js
+++ React.js/react-disney-plus-app/src/components/Nav.js
@@ -118,14 +118,13 @@ const Nav = () => {
 }
 
 export default Nav
-
 const DropDown = styled.div`
   position: absolute;
   top: 48px;
   right: 0px;
-  background: rgb(19, 19, 19);
+  background: rgb(19, 19, 19)
   border: 1px solid rgba(151, 151, 151, 0.34);
-  border-radius: 4px;
+  border-radius:  4px;
   box-shadow: rgb(0 0 0 /50%) 0px 0px 18px 0px;
   padding: 10px;
   font-size: 14px;
