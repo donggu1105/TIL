84d19c50786ee8b6abf1767bbc7462099b87a33e
donggu1105 <kbh11051@gmail.com>
2023-10-02 16:34:34 +0900

    nextjs

--- React.js/react-disney-plus-app/src/pages/LoginPage/index.js
+++ React.js/react-disney-plus-app/src/pages/LoginPage/index.js
@@ -7,7 +7,7 @@ const LoginPage = () => {
             <Content>
                 <Center>
                     <LogoOne src="/images/cta-logo-one.svg" alt="logo-one" />
-                    <SignUpLink>지금 가입</SignUpLink>
+                    {/*<SignUpLink>지금 가입</SignUpLink>*/}
                     <Description>
                         영화에 대한 프리미어 액세스를 얻으십시오.
                         디즈니 플러스 가격은 다음 주부터 1000원 인상됩니다.
