689006e9e274c694f0e379f374d7debc06ba3218
donggu1105 <kbh11051@gmail.com>
2023-11-01 23:02:49 +0900

    publishing

--- React.js/react-tailwind-app/src/components/Support.js
+++ React.js/react-tailwind-app/src/components/Support.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import supportImg from '../assets/support.jpeg';
 
 const Support = () => {
     return (
-        <div>
-            
+        <div name="support" className="w-full m-24">
+
+            <div className="w-full h-[700px] bg-slate-900/90 absolute">
+                <img className='object-cover w-full h-full mix-blend-overlay' src={supportImg} alt="/" />
+            </div>
+
+            <div className='max-w-[1240px] mx-auto text-white relative'>
+
+            </div>
         </div>
     );
 };
